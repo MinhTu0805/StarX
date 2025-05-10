018beff869eacdc4dd40bedab0ee7dfbfc262d5d
MinhTu0805 <129403783+MinhTu0805@users.noreply.github.com>
2025-05-10 19:06:16 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const menuIcon = document.querySelector('#menu-icon');
-const navbar = document.querySelector('.navbar');
-const navbg = document.querySelector('.nav-bg');
-
-menuIcon.addEventListener('click', () => {
-    menuIcon.classList.toggle('bx-x'); // Đổi icon thành dấu "X" khi mở menu
-    navbar.classList.toggle('active');
-    navbg.classList.toggle('active');
-});
